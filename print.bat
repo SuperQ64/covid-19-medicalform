e7aa06b4ec6802dd94f776e24546d131e974fddc
SuperQ <asiamoth64@gmail.com>
2021-09-11 13:57:39 +0900

    変数の削除

--- print.bat
+++ print.bat
@@ -1,11 +1,6 @@
-set ACRRD_PATH="C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe"
-set PDF_PATH=%~dp0/out/%1
-set PRINTER=%2
-set DRIVER=%3
-set PORT=%4
+@REM ACRRD_PATH = "C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe"
+@REM PDF_PATH = %~dp0/out/%1  PRINTER_NAME = %2   DRIVER_NAME = %3    PORT_NAME = %4
 
-start "" %ACRRD_PATH% /t %PDF_PATH% %PRINTER% %DRIVER% %PORT%
-
+start "" "C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe" /t %~dp0/out/%1 %2 %3 %4
 timeout /t 10
-
 taskkill /F /IM AcroRd32.exe
