c4fd446836ebc4769eb325f7d5fc0e46ef3a7c72
SuperQ <asiamoth64@gmail.com>
2021-09-11 14:38:46 +0900

    Windows用URL置換処理追加

--- covid19-medicalform/qr_read.py
+++ covid19-medicalform/qr_read.py
@@ -1,12 +1,14 @@
 import json
 import medical_form as mf
 import medical_form_print as mfp
+import platform
 
 def main():
     while True:
         print('QRコードをかざしてください')
         url = input()   #https://****/{接種券番号}
-        url.replace('+',':')
+        if platform.system() == 'Windows':
+            url = replace_url(url)
         div_url = url.split('/')
         id = div_url[len(div_url) - 1]
         file_name = id + '.pdf'
@@ -23,6 +25,14 @@ def main():
         mfp.run_print(file_name=file_name)
     
     print('終了します')
+
+def replace_url(url):
+    list = [['+',':'],['=','_']]
+    fixed_url = url
+    for pair in list:
+        fixed_url = fixed_url.replace(pair[0],pair[1])
+
+    return fixed_url
 
 if __name__ == '__main__':
     main()
