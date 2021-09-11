48dd6c4f3494d5ed93cc263e62e5864625eec0e4
SuperQ <asiamoth64@gmail.com>
2021-09-11 13:57:07 +0900

    書き込み位置調整

--- covid19-medicalform/medical_form.py
+++ covid19-medicalform/medical_form.py
@@ -30,74 +30,74 @@ def write_q1():
         cc.drawString(340,616,answer[3])
         cc.drawString(375,616,answer[4])
 
-    write_yes_no(442,625,answer[0])  
+    write_yes_no(441,624,answer[0])  
     return
 
 def write_q4():
     answer = form_info['q4']
     if answer[0] == 'true':
-        write_checkmark(38,550,answer[1])
-        write_checkmark(118,550,answer[2])
-        write_checkmark(181,550,answer[3])
-        write_checkmark(246,550,answer[4])
-        write_checkmark(38,537,answer[5])
+        write_checkmark(38,548,answer[1])
+        write_checkmark(118,548,answer[2])
+        write_checkmark(181,548,answer[3])
+        write_checkmark(246,548,answer[4])
+        write_checkmark(38,535,answer[5])
         cc.drawString(150,535,answer[6])
 
-    write_yes_no(442,550,answer[0])
+    write_yes_no(441,549,answer[0])
 
 def write_q5():
     answer = form_info['q5']
     if answer[0] == 'true':
-        write_checkmark(86,497,answer[1])
-        write_checkmark(132,497,answer[2])
-        write_checkmark(181,497,answer[3])
-        write_checkmark(228,497,answer[4])
-        write_checkmark(284,497,answer[5])
-        write_checkmark(384,497,answer[6])
-        write_checkmark(86,480,answer[7])
+        write_checkmark(85,497,answer[1])
+        write_checkmark(131,497,answer[2])
+        write_checkmark(180,497,answer[3])
+        write_checkmark(227,497,answer[4])
+        write_checkmark(283,497,answer[5])
+        write_checkmark(383,497,answer[6])
+        write_checkmark(85,480,answer[7])
         cc.drawString(130,480,answer[8])
-        write_checkmark(86,463,answer[9])
+        write_checkmark(85,463,answer[9])
         cc.drawString(190,463,answer[10])
         write_checkmark(286,463,answer[11])
         cc.drawString(330,463,answer[12])
 
-    write_yes_no(442,492,answer[0])
+    write_yes_no(441,490,answer[0])
 
 
 def write_q6():
     answer = form_info['q6']
     if answer[0] == 'true':
-        cc.drawString(300,441,answer[1])
+        cc.drawString(300,440,answer[1])
 
-    write_yes_no(442,442,answer[0])
+    write_yes_no(441,440,answer[0])
 
 def write_q7():
     answer = form_info['q7']
     if answer[0] == 'true':
         cc.drawString(240,420,answer[1])
 
-    write_yes_no(442,422,answer[0])
+    write_yes_no(441,421,answer[0])
 
 def write_q9():
     answer = form_info['q9']
     if answer[0] == 'true':
         cc.drawString(170,370,answer[1])
 
-    write_yes_no(442,377,answer[0])
+    write_yes_no(441,377,answer[0])
 
 def write_q10():
     answer = form_info['q10']
     if answer[0] == 'true':
         cc.drawString(70,342,answer[1])
         cc.drawString(280,342,answer[2])
-    write_yes_no(442,350,answer[0])
+    write_yes_no(441,349,answer[0])
 
 def write_q12():
     answer = form_info['q12']
     if answer[0] == 'true':
-        cc.drawString(227,304,answer[1])
-        cc.drawString(400,304,answer[2])
-    write_yes_no(442,305,answer[0])
+        cc.drawString(225,304,answer[1])
+        cc.drawString(398,304,answer[2])
+    write_yes_no(441,305,answer[0])
 
 def write_name(x,y,name):
     cc.setFont(fontname_g,11)
@@ -164,22 +164,22 @@ def write(info,file_out):
     cc.setFont(fontname_g,12)
     cc.drawString(65,710,form_info['furigana'])
     if form_info['sex'] == '男':
-        cc.drawString(338,669,'✓')
+        cc.drawString(337,668,'✓')
     else:
-        cc.drawString(373,669,'✓')
+        cc.drawString(372,668,'✓')
     write_q1()
-    write_yes_no(442,600,form_info['q2'])
-    write_yes_no(442,580,form_info['q3'])
+    write_yes_no(441,600,form_info['q2'])
+    write_yes_no(441,580,form_info['q3'])
     write_q4()
     write_q5()
     write_q6()
     write_q7()
-    write_yes_no(442,402,form_info['q8'])
+    write_yes_no(441,402,form_info['q8'])
     write_q9()
     write_q10()
-    write_yes_no(442,325,form_info['q11'])
+    write_yes_no(441,325,form_info['q11'])
     write_q12()
-    write_yes_no(442,285,form_info['q13'])
+    write_yes_no(441,285,form_info['q13'])
 
     if form_info['agreement'] == 'true':
         cc.drawString(377,186,'✓')
